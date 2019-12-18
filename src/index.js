200739fbb6ca6add5b48b15b9b0aa06e7684569c
Strandberg Filip <strandbergfilip94@gmail.com>
2019-12-18 14:08:10 +0100

    Component hierarchy comment

--- src/index.js
+++ src/index.js
@@ -6,5 +6,22 @@ import './index.css';
 import ChartHandler from './components/chart_handler/chart_handler';
 // import PostItHandler from './components/post_it_handler/post_it_handler';
 
+/*
+https://reactjs.org/docs/thinking-in-react.html
+
+- ChartHandler
+  - ChartContainer
+    - Chart
+  - ChartInputHandler
+    - InputContainer (?)
+      - NumberInput
+      - NumberInput
+    - InputContainer (?)
+      - Button
+
+- PostItHandler
+  - TBD
+*/
+
 ReactDOM.render(<ChartHandler />, document.getElementById('left-container'));
 // ReactDOM.render(<PostItHandler />, document.getElementById('right-container'));
