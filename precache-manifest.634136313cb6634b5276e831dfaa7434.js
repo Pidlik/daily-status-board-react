6b930372d6c491c3419813c6f61cdd3b80ed162b
Strandberg Filip <strandbergfilip94@gmail.com>
2019-12-19 08:15:16 +0100

    Updates

--- precache-manifest.634136313cb6634b5276e831dfaa7434.js
+++ precache-manifest.634136313cb6634b5276e831dfaa7434.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d57828af2730033d22359b630d070d43",
+    "revision": "c4e77558f10d9a603bcdb3d14e305f8d",
     "url": "/daily-status-board-react/index.html"
   },
   {
-    "revision": "3c1c8a684e5958f94121",
+    "revision": "8c17a44b8408cdfd5cc2",
     "url": "/daily-status-board-react/static/css/main.6f60015b.chunk.css"
   },
   {
-    "revision": "869de581e01e8b957b43",
-    "url": "/daily-status-board-react/static/js/2.a7bf7c11.chunk.js"
+    "revision": "050944f57f874b2179a9",
+    "url": "/daily-status-board-react/static/js/2.0d3e7db3.chunk.js"
   },
   {
     "revision": "fc9dba852eaddf531193be703e8b0310",
-    "url": "/daily-status-board-react/static/js/2.a7bf7c11.chunk.js.LICENSE"
+    "url": "/daily-status-board-react/static/js/2.0d3e7db3.chunk.js.LICENSE"
   },
   {
-    "revision": "3c1c8a684e5958f94121",
-    "url": "/daily-status-board-react/static/js/main.3fecd984.chunk.js"
+    "revision": "8c17a44b8408cdfd5cc2",
+    "url": "/daily-status-board-react/static/js/main.f8c4921d.chunk.js"
   },
   {
     "revision": "ee5bf5d0637b26b038ae",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daily-status-board-react/precache-manifest.76a4f5a644d2a48b295e6b195bfd4236.js"
+  "/daily-status-board-react/precache-manifest.634136313cb6634b5276e831dfaa7434.js"
 );
 
 self.addEventListener('message', (event) => {
