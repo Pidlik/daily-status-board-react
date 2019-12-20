92f322d36c4c6bcce4ab3ee77bdca5fd25df5876
Filip <strandbergfilip94@gmail.com>
2019-12-20 22:58:40 +0100

    Add trashcan image

--- src/components/PostItControls.js
+++ src/components/PostItControls.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './PostItControls.css'
 
+import img from '../assets/images/trashcan.png'
+
 function AddPostItButton(props) {
   return(
     <button className="add-post-it">Add Post It</button>
@@ -10,13 +12,13 @@ function AddPostItButton(props) {
 
 function Trashcan(props) {
   // https://stackoverflow.com/questions/30730369/reactjs-component-not-rendering-textarea-with-state-variable
+  // onDragEnter="drag_enter(event)"
+  // onDragLeave="drag_leave(event)"
+  // onDrop="drop_trashcan(event)"
+  // onDragOver="drag_over(event)">
   return(
-    <div id="trashcan-container" dragable="false"
-      ondragenter="drag_enter(event)"
-      ondragleave="drag_leave(event)"
-      ondrop="drop_trashcan(event)"
-      ondragover="drag_over(event)">
-        <img className="trashcan-image" alt="trashcan" />
+    <div className="trashcan-container" dragable="false">
+        <img className="trashcan-image" src={img} alt="trashcan" />
     </div>
   );
 }
